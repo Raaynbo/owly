64b4d705ac1e3e351689d4f4a635735e31091c8d
Raaynbo <rayan.boudjema@protonmail.com>
2024-09-28 16:32:44 +0200

    modified dir management to gain readability in dir

---
+++ src/index.js
@@ -0,0 +1,16 @@
+
+console.log("index.js")
+
+
+const content = document.querySelector(".app");
+
+
+const app = function (){
+	
+}
+
+
+
+content.textContent = "OWLY TASK TRACKER";
+
+
---
+++ src/js/login.js
@@ -0,0 +1,9 @@
+const taskName = "taskname";
+const taskDescription = "Description";
+const taskPriority = 0;
+const taskDueDate ="date";
+
+function createTask(){
+	
+
+}
