fc8b4d94e2af9fbbd176ca975356c53b81f3ec90
Raaynbo <rayan.boudjema@protonmail.com>
2024-10-16 12:42:22 +0200

    feat-popup: popup now display a form with input validation to check if they are not empty

--- src/js/components/popup.js
+++ src/js/components/popup.js
@@ -34,6 +34,7 @@ function createModal(title="OWLY GOT INFOS FOR YOU", attribute=[]){
 		switch(attr){
 			case "form":
 				console.log("form")
+				createForm(modal_content)
 				break;
 			case "action":
 				console.log("action")
@@ -48,6 +49,60 @@ function createModal(title="OWLY GOT INFOS FOR YOU", attribute=[]){
 	document.body.appendChild(overlay);
 }
 
+
+function createForm(container){
+	const form = document.createElement("form");
+	const labelname = document.createElement('label');
+	const labeldate = document.createElement('label');
+	const labelpriority = document.createElement('label');
+	const inputpriority = document.createElement('input');
+	const inputname = document.createElement('input');
+	const inputdate = document.createElement('input');
+	const inputsubmit = document.createElement('input');
+	const inputList = [];
+
+	labelname.textContent = "Name \t";
+	labeldate.textContent = "Due Date \t";
+	labelpriority.textContent = "Priority \t";
+	inputname.type = "text";
+	inputdate.type = "date";
+	inputpriority.type = "range";
+	inputsubmit.type = "button";
+	inputsubmit.value = "button";
+	inputpriority.min = 0;
+	inputpriority.max = 3;
+	
+	inputList.push(inputname);
+	inputList.push(inputdate);
+	inputList.push(inputpriority);
+	
+	inputsubmit.addEventListener("click", (e)=>{
+		if(!verifyInput(inputList)){
+			return false;	
+		}
+		// user.createProject()
+
+	})
+	
+	labelname.appendChild(inputname);
+	labeldate.appendChild(inputdate);
+	labelpriority.appendChild(inputpriority);
+	form.appendChild(labelname);
+	form.appendChild(labeldate);
+	form.appendChild(labelpriority);
+	form.appendChild(inputsubmit);
+	container.appendChild(form);
+}
+
+function verifyInput(list){
+	let isFilled = true;
+	list.forEach((el)=>{
+		if (el.value == "" || el.value == undefined){
+			isFilled = false;
+		}
+	})
+	return isFilled;
+}
 
 function closeModal(){
 	const modal = document.querySelector(".modal");
