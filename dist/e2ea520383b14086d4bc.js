6d5d1af8a40983a68cc501d5357b9781eafc1926
Raaynbo <rayan.boudjema@protonmail.com>
2024-10-22 13:40:47 +0200

    things

---
+++ dist/e2ea520383b14086d4bc.js
@@ -0,0 +1,65 @@
+import "./css/style.css";
+import "./css/components.css";
+import {introduction, startATask} from "./js/utils.js";
+import {User} from "./js/user.js";
+import {homeView} from "./views/home.js";
+import {detailView} from "./views/detail.js";
+import {actionView} from "./views/action_button.js";
+import {localStore} from "./js/components/storage.js";
+
+
+
+const content = document.querySelector(".app");
+const action_btn = document.querySelector(".action_button");
+const appInfo = {
+	page:"home",
+	project:"",
+	task: ""
+}
+
+const user = new User("Raaynbo");
+
+
+action_btn.addEventListener("click", (e) => {
+		console.log("over")
+		actionView(user);	
+		appInfo.page = "form";
+	})
+
+const app = function (){
+	if (localStore()){
+		console.log("introduction")
+		introduction(user);
+	}else{
+		user.loadTask()
+	}
+
+	user.page="home";
+
+	renderApp(user);
+}
+
+function renderApp(user){
+	clearApp();
+	
+	switch(user.page){
+		case "home":
+			homeView(content, user);
+			break;
+		case "detail":
+			detailView(content, user);
+			break;
+	}
+	console.log(appInfo)
+}
+
+
+function clearApp(){
+	while (content.firstChild){
+		content.removeChild(content.lastChild);
+	}
+}
+
+app();
+
+export {renderApp};
